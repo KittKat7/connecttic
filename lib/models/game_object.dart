2a25f0575b88c4fc970c58744e3b8bf8362dcc82
KittKat <122490347+KittKat7@users.noreply.github.com>
2024-11-12 09:05:52 -0500

    Removed unused import and added doc comments for game_object

--- lib/models/game_object.dart
+++ lib/models/game_object.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:kittkatflutterlibrary/kittkatflutterlibrary.dart';
 
+/// Abstract parent class for objects which are displaye on the game board.
+/// [getTile] and [getBigTile] return the apprpriate tiles for the object being displayed.
 abstract class GameObject {
+  /// Returns the current tile as a widget.
   Widget getTile();
+
+  /// Returns a larger version of the current tile, scaled to fit within a certain size.
   Widget getBigTile() {
     return Transform.scale(
       scale: 1.25,
@@ -11,7 +15,9 @@ abstract class GameObject {
   }
 }
 
+/// Represents a blocker object that can be used to block player movement or other game logic.
 class BlockerObject extends GameObject {
+  /// The tile displayed when the blocker is active, scaled down from its original size.
   static Widget _tile = Transform.scale(
       scale: 0.75,
       child: const ColorFiltered(
@@ -22,6 +28,7 @@ class BlockerObject extends GameObject {
             filterQuality: FilterQuality.none,
           )));
 
+  /// Sets the color of the blocker's tile to a specific value.
   static setColor(Color c) {
     _tile = Transform.scale(
         scale: 0.75,
@@ -34,6 +41,7 @@ class BlockerObject extends GameObject {
             )));
   }
 
+  /// Returns the current tile as a widget.
   @override
   Widget getTile() {
     return _tile;
