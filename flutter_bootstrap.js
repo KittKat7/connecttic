ba7ae1d122aae2d3f91e6188a5f3a4ed98b8e089
KittKat7 <KittKat7@users.noreply.github.com>
2024-11-12 22:49:54 +0000

    deploy: 1edbbbab958c5e800e2ebe9002106ac8bb1ddbc4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1604423692"
+    serviceWorkerVersion: "3502781184"
   }
 });
