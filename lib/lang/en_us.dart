d1d6bb3df4e2b80b90020d164f98b83908bcf752
KittKat <122490347+KittKat7@users.noreply.github.com>
2024-12-21 21:14:37 -0500

    Resolves #1 fixed spelling for "challenging" and "introduces"

--- lib/lang/en_us.dart
+++ lib/lang/en_us.dart
@@ -29,8 +29,8 @@ const Map<String, String> en_us = {
 ConnectTic is a mashup of Tic-Tac-Toe and Connect 4. The goal of the game is to get 4 pieces
 connected in a row, column, or diagonal (similar to Connect 4). To accomplish this, you can play on
 any empty tile on the board (similar to Tic-Tac-Toe). There is a catch, you cannot play on any tile
-directly next to your last played piece. This twist makes the game much more challanging and
-introduced new stratagies into the mix. Have fun!!!
+directly next to your last played piece. This twist makes the game much more challenging and
+introduces new stratagies into the mix. Have fun!!!
 """,
   "txtAbout": """
 ## About ConnectTic
