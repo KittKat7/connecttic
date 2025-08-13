750466b77642488f38be966d4271d2dbeb959590
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-08-13 09:18:17 -0400

    game model rewrite

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -1,172 +1,204 @@
-import 'dart:async';
+import 'pos.dart';
 
-import 'package:connecttic/models/board.dart';
-import 'package:connecttic/models/computer_player.dart';
-import 'package:connecttic/models/player.dart';
-import 'package:kittkatflutterlibrary/kittkatflutterlibrary.dart';
+/// The state of the game
+enum GameStatus {
+  draw,
+  player1,
+  player2,
+  playing,
+  idle,
+}
 
-/// Game class manages the game state/ players, current turn, and win status.
+/// Game object, is the state of the current game
 class Game {
-  /// The board which is used for this game.
-  Board board;
+  static const int empty = 0;
+  static const int p1 = 1; // 1 => 11
+  static const int p2 = 2; // 2 => 12
 
-  /// The list of players in the game.
-  PlayerList players;
-  List<List<int>>? winset;
+  // Convert a given player id to its super version (last played)
+  static int sup(int id) => id < 10 && id > 0 ? id + 10 : id;
+  // Convert a given player id to its sub version (normal)
+  static int sub(int id) => id > 10 ? id - 10 : id;
 
-  /// Bool flag, true if the game has ended.
-  bool isEnded = false;
+  /// A matrix storing the items on the board
+  List<List<int>> board;
 
-  Timer? _timer;
-  int gameRuntime;
+  /// The current player (1 or 2)
+  int currentPlayer;
 
-  void Function()? onEndCallback;
-  void Function()? onUpdateBoard;
-
-  /// Tracks the current player.
-  Player _currentPlayer;
-
-  final Map<String, String> _gameInfo;
+  /// Game status
+  GameStatus status;
 
   /// Constructor
-  Game(this.board, this.players)
-      : _currentPlayer = players.get(1),
-        _gameInfo = {},
-        gameRuntime = 0,
-        onEndCallback = null {
-    // Initiate the timer
-    _timer = Timer.periodic(const Duration(seconds: 1), _onTimeUpdate);
-    if (!isEnded && _currentPlayer is ComputerPlayer) {
-      (_currentPlayer as ComputerPlayer).computerPlay(this);
-    }
-  }
+  Game({int width = 7, int height = 6})
+      : board = List.generate(width, (_) => List.filled(height, 0)),
+        currentPlayer = 1,
+        status = GameStatus.idle;
 
-  /// The function to be run every second when the timer updates.
-  void _onTimeUpdate(time) {
-    // If the game has ended, stop the timer and return
-    if (isEnded) {
-      _timer?.cancel();
-      return;
-    }
-    gameRuntime++;
-  }
+  /// Returns the json objecct for the game
+  Map<String, dynamic> toJson() => {
+        'board': board,
+        'currentPlayer': currentPlayer,
+        'status': status.index,
+      };
 
-  /// Dispose of the game, cancel the timer and dispose of the time notifier.
-  void dispose() {
-    _timer?.cancel();
-    isEnded = true;
-  }
+  /// Returns a game from a json map
+  Game.fromJson(Map<String, dynamic> json)
+      : board = json['board'],
+        currentPlayer = json['currentPlayer'],
+        status = GameStatus.values[json['status']];
 
-  /// Sets the callback function which is run when the game ends.
-  void setOnEndCallback(void Function() func) {
-    onEndCallback = func;
-  }
-
-  /// Sets the function which is run when the board is updated. This should be a setstate function
-  /// which will be called so the displayed board will be updated.
-  void setOnUpdateBoard(void Function() func) {
-    onUpdateBoard = func;
-  }
-
-  /// Returns the current player (the player whose turn it is).
-  Player getCurrentPlayer() {
-    return _currentPlayer;
-  }
-
-  Player getPlayerAtPosition(int x, int y) {
-    if (!board.tileIsPlayer(x, y)) throw Exception();
-    if (board.get(x, y) > 9) return players.get(board.get(x, y) ~/ 10);
-    return players.get(board.get(x, y));
-  }
-
-  /// Cycles [_currentPlyer] to be the next player in the list.
-  void iterateCurrentPlayer() {
-    _currentPlayer = players.nextPlayer(_currentPlayer);
-  }
-
-  /// Manages the functions needed to be run when a player plays. This method runs when a player
-  /// tries to play.
-  /// @returns true if the play is successful
-  bool play(int x, int y) {
-    // If the selected tile is NOT an EmptyTile, its either a blocker or a player, return and don't
-    // complete the play.
-    if (isEnded || !board.tileIsEmpty(x, y)) return false;
-
-    // If the current players last x and y are not negative (they have played before) then un
-    // highlight their last play and unset their last play.
-    if (_currentPlayer.lastx > -1 && _currentPlayer.lasty > -1) {
-      board.setLastPlay(_currentPlayer.lastx, _currentPlayer.lasty, false);
-    }
-
-    // Set the tile at the play location to be a copy of the player's tile.
-    board.set(x, y, players.getNum(_currentPlayer));
-
-    // If the board finds a win condition, mark the game as ended and skip the rest of the play
-    // functions.
-    winset = board.gameIsWon();
-    if (winset != null) {
-      isEnded = true;
-      onGameEnd(_currentPlayer);
-    }
-
-    // Set the play to be highlighted, update the last x and y, and iterate the current player.
-    _currentPlayer.lastx = x;
-    _currentPlayer.lasty = y;
-    iterateCurrentPlayer();
-
-    // If the new current player has played previously, mark their last play by putting blockers
-    // around it.
-    if (_currentPlayer.lastx > -1 && _currentPlayer.lasty > -1) {
-      board.setLastPlay(_currentPlayer.lastx, _currentPlayer.lasty, true);
-    }
-
-    if (!isEnded && _currentPlayer is ComputerPlayer) {
-      (_currentPlayer as ComputerPlayer).computerPlay(this);
-    }
-    if (onUpdateBoard != null) onUpdateBoard!();
-
-    // Check if their are playable tiles left on the board. If there are no more playable tiles, the
-    // game has ended in a draw.
-    if (!board.hasOpenTile()) {
-      isEnded = true;
-      onGameEnd(null);
+  // Validation check to make sure the given position is in bounds of the board
+  bool _isInBounds(Pos pos) {
+    int x = pos.x;
+    int y = pos.y;
+    // Out of bounds - return false
+    if (x < 0 || x >= board.length || y < 0 || y >= board[x].length) {
+      return false;
     }
     return true;
   }
 
-  void onGameEnd(Player? winner) {
-    _gameInfo["winner"] =
-        winner != null ? getLang('pmtPlayer', [players.getNum(winner)]) : getLang("pmtDraw");
-    int gameTime = gameRuntime;
-    _gameInfo["time"] = getLang('mscMinSec',
-        [(gameTime ~/ 60).toString().padLeft(2, '0'), (gameTime % 60).toString().padLeft(2, '0')]);
-
-    if (onEndCallback != null) onEndCallback!();
+  /// Returns true if the given location on the board is empty (0), false
+  /// otherwise
+  bool isPlayable(Pos pos) {
+    // Out of bounds check
+    if (!_isInBounds(pos)) return false;
+    int x = pos.x;
+    int y = pos.y;
+    // Location is not empty
+    if (board[x][y] != 0) {
+      return false;
+    }
+    // Location is next to last played
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        // If current tile, continue
+        if (i == 0 && j == 0) continue;
+        // If the adjacent tile is the super version
+        if (_isInBounds(Pos(x: x + i, y: y + j)) &&
+            board[x + i][y + j] == sup(currentPlayer)) {
+          return false;
+        }
+      }
+    }
+    // Passed all fail conditions, return true
+    return true;
   }
 
-  Map<String, String> getGameInfo() {
-    return Map.from(_gameInfo);
+  void updateStatus() {
+    // Keep track of if there are still playable tiles
+    bool playable = false;
+    for (int x = 0; x < board.length; x++) {
+      for (int y = 0; y < board[x].length; y++) {
+        // If there is a playable tile, track it so the game does not end in a
+        // draw
+        if (!playable && isPlayable(Pos(x: x, y: y))) playable = true;
+        // Check for row win
+        if (_isFourInARow(Pos(x: x, y: y), 1, 0)) {
+          board[x][y] = board[x + 1][y] =
+              board[x + 2][y] = board[x + 3][y] = sup(board[x][y]);
+          status =
+              sub(board[x][y]) == p1 ? GameStatus.player1 : GameStatus.player2;
+          return;
+        }
+        // Check for column
+        if (_isFourInARow(Pos(x: x, y: y), 0, 1)) {
+          board[x][y] = board[x][y + 1] =
+              board[x][y + 2] = board[x][y + 3] = sup(board[x][y]);
+          status =
+              sub(board[x][y]) == p1 ? GameStatus.player1 : GameStatus.player2;
+          return;
+        }
+        // Check for diag right
+        if (_isFourInARow(Pos(x: x, y: y), 1, 1)) {
+          board[x][y] = board[x + 1][y + 1] =
+              board[x + 2][y + 2] = board[x + 3][y + 3] = sup(board[x][y]);
+          status =
+              sub(board[x][y]) == p1 ? GameStatus.player1 : GameStatus.player2;
+          return;
+        }
+        // Check for diag left
+        if (_isFourInARow(Pos(x: x, y: y), -1, 1)) {
+          board[x][y] = board[x - 1][y + 1] =
+              board[x - 2][y + 2] = board[x - 3][y + 3] = sup(board[x][y]);
+          status =
+              sub(board[x][y]) == p1 ? GameStatus.player1 : GameStatus.player2;
+          return;
+        }
+      }
+    }
+    // If there are no playable tiles, set status to draw
+    if (!playable) {
+      status = GameStatus.draw;
+    }
   }
-}
 
-class PlayerList {
-  final List<Player> _players;
-  PlayerList() : _players = [];
-
-  Player get(int number) => _players[number - 1];
-  void set(int number, Player player) => _players[number] = player;
-  int add(Player player) {
-    _players.add(player);
-    return _players.length;
+  /// Check if there are four in a row of the same player. [pos] is the starting
+  /// position, and [dx] and [dy] (delta x, delta y) are the amount to change in
+  /// those directions. Returns true if there are four in a row, false
+  /// otherwise.
+  bool _isFourInARow(Pos pos, int dx, int dy) {
+    int x = pos.x;
+    int y = pos.y;
+    // Boundary check for starting, and make sure starting is actually a player
+    // tile
+    if (!_isInBounds(pos) || board[x][y] == empty) return false;
+    // For every item in the row/col/diag of 4
+    for (int i = 1; i < 4; i++) {
+      // Set the check x and check y values
+      int cx = x + dx * i;
+      int cy = y + dy * i;
+      // If cx,cy is out of bounds, or cx,cy is not the same player as x,y then
+      // its not four in a row, return false
+      if (!_isInBounds(Pos(x: cx, y: cy)) ||
+          sub(board[x][y]) != sub(board[cx][cy])) {
+        return false;
+      }
+    }
+    // Otherwise return true
+    return true;
   }
 
-  int getNum(Player player) => _players.indexOf(player) + 1;
+  /// Starts the game by setting the status to playing from idle. Returns true
+  /// if the status is set, false otherwise.
+  bool startGame() {
+    if (status != GameStatus.idle) return false;
+    status = GameStatus.playing;
+    return true;
+  }
 
-  PlayerList.fromList(List<Player> players) : _players = players;
+  /// Unset the super for the current player. This is run before the new super
+  /// tile is played.
+  void _unsetSuper() {
+    // Go through every tile on the board
+    for (int x = 0; x < board.length; x++) {
+      for (int y = 0; y < board[x].length; y++) {
+        // Unset super tiles for current player
+        if (board[x][y] == sup(currentPlayer)) board[x][y] = sub(currentPlayer);
+      }
+    }
+  }
 
-  int get length => _players.length;
+  bool play(Pos pos) {
+    // Check if game is in playing status
+    if (status != GameStatus.playing) return false;
+    // Check if the given tile is playable
+    if (!isPlayable(pos)) return false;
 
-  Player nextPlayer(Player player) => _players.indexOf(player) + 1 < _players.length
-      ? _players[_players.indexOf(player) + 1]
-      : _players[0];
+    // Unset any previous super/last play tiles for current player
+    _unsetSuper();
+
+    // Set the play
+    board[pos.x][pos.y] = sup(currentPlayer);
+
+    // Cycle player
+    currentPlayer = (currentPlayer + 1) % 2;
+
+    // Update the game status/check for a win/end condition
+    updateStatus();
+
+    // Return true, the play was a success
+    return true;
+  }
 }
