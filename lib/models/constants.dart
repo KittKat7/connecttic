451952aff4b63d9fc4c613d01e07f07dc978f238
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-11-13 14:56:58 -0500

    Removed hardcoded true for prod

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -1,2 +1,2 @@
 /// Whether the app/server is running in release mode
-const bool isRelease = bool.fromEnvironment('dart.vm.product') || true;
+const bool isRelease = bool.fromEnvironment('dart.vm.product');
